9cfe3eee04578f5d72f2b07444829094c2bd546c
LiudmilaLobanova <liudmilanl@gmail.com>
2023-03-14 00:01:50 -0700

    First simple tests added

--- src/test/java/ru/netology/cardDelivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/cardDelivery/CardDeliveryTest.java
@@ -1,16 +1,22 @@
 package ru.netology.cardDelivery;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import static com.codeborne.selenide.Selenide.open;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
@@ -42,7 +48,24 @@ public class CardDeliveryTest {
     void ShouldOpenWindow() {
 
         Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "800x1000";
         open("http://localhost:9999/");
+
+        $x("//input[@placeholder='Город']").setValue("Москва");
+//        $(withText("Город")).setValue("Москва");
+//        $("[data-test-id=city]").setValue("Москва");
+//        driver.findElement(By.cssSelector("input [data-test-id=city]")).sendKeys("Москва");
+        $x("//input[@name='name']").setValue("Ирина Мордвинова");
+        $x("//input[@name='phone']").setValue("+74991112233");
+        $("[data-test-id=agreement]").click();
+        $x("//*[contains(text(), 'Забронировать')]").click();
+        $x("//*[contains(text(), 'Успешно!')]").should(Condition.appear, Duration.ofSeconds(15));
+
+
+
+
+
+
 
     }
 
