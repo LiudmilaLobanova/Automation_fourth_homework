5a64bd852f40e6f9b9f731afe49ba286abe68d1e
LiudmilaLobanova <liudmilanl@gmail.com>
2023-03-18 19:01:45 -0700

    Headless options was edited

--- src/test/java/ru/netology/cardDelivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/cardDelivery/CardDeliveryTest.java
@@ -37,8 +37,11 @@ public class CardDeliveryTest {
 
     @BeforeEach
     void setUp() {
-
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
         Configuration.headless = true;
         open("http://localhost:9999/");
 
