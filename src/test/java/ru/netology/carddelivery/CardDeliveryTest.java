9fec5f0b08d42fa48e327773122b97a03aa28df5
LiudmilaLobanova <liudmilanl@gmail.com>
2023-03-21 13:43:23 -0700

    Major improvements after the first comments

--- src/test/java/ru/netology/carddelivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/carddelivery/CardDeliveryTest.java
@@ -1,14 +1,12 @@
-package ru.netology.cardDelivery;
+package ru.netology.carddelivery;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selectors;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,68 +16,49 @@ import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
     private WebDriver driver;
 
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
     @BeforeEach
     void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
         Configuration.headless = true;
         open("http://localhost:9999/");
-
-
     }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
-
+public String generateDate(long addDays, String pattern){
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+}
     @Test
     void ShouldBeSuccess() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(4,"dd.MM.yyyy");
 
         $x("//input[@placeholder='Город']").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue((ltdFormat));
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue((planningDate));
         $x("//input[@name='name']").setValue("Олег-Тиньков");
         $x("//input[@name='phone']").setValue("+74991112233");
         $("[data-test-id=agreement]").click();
         $x("//*[contains(text(), 'Забронировать')]").click();
         $x("//*[contains(text(), 'Успешно!')]").should(Condition.appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate)).shouldBe(Condition.visible);
 
     }
 
     @Test
     void shouldNotBeSuccessLessTimeToDelivery() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(2);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(2,"dd.MM.yyyy");
 
         $x("//input[@placeholder='Город']").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(ltdFormat);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("Олег-Тиньков");
         $x("//input[@name='phone']").setValue("+74991112233");
         $("[data-test-id=agreement]").click();
@@ -90,13 +69,12 @@ public class CardDeliveryTest {
 
     @Test
     void shouldNotBeSuccessWrongCity() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(4,"dd.MM.yyyy");
 
         $x("//input[@placeholder='Город']").setValue("Лос-Анжелес");
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(ltdFormat);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("Олег-Тиньков");
         $x("//input[@name='phone']").setValue("+74991112233");
         $("[data-test-id=agreement]").click();
@@ -106,13 +84,12 @@ public class CardDeliveryTest {
 
     @Test
     void shouldNotBeSuccessEmptyCity() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(4,"dd.MM.yyyy");
 
         $x("//input[@placeholder='Город']").setValue("");
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(ltdFormat);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("Олег-Тиньков");
         $x("//input[@name='phone']").setValue("+74991112233");
         $("[data-test-id=agreement]").click();
@@ -122,13 +99,12 @@ public class CardDeliveryTest {
 
     @Test
     void shouldNotBeSuccessWrongLatinName() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(4,"dd.MM.yyyy");
 
         $x("//input[@placeholder='Город']").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(ltdFormat);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("Elon Musk");
         $x("//input[@name='phone']").setValue("+74991112233");
         $("[data-test-id=agreement]").click();
@@ -138,13 +114,12 @@ public class CardDeliveryTest {
 
     @Test
     void shouldNotBeSuccessWrongNameWithDigit() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(4,"dd.MM.yyyy");
 
         $x("//input[@placeholder='Город']").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(ltdFormat);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("X Æ A-12");
         $x("//input[@name='phone']").setValue("+74991112233");
         $("[data-test-id=agreement]").click();
@@ -154,13 +129,12 @@ public class CardDeliveryTest {
 
     @Test
     void shouldNotBeSuccessWrongNameWithSpecialCharacter() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(4,"dd.MM.yyyy");
 
         $x("//input[@placeholder='Город']").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(ltdFormat);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("Олег-Тиньков$");
         $x("//input[@name='phone']").setValue("+74991112233");
         $("[data-test-id=agreement]").click();
@@ -170,13 +144,12 @@ public class CardDeliveryTest {
 
     @Test
     void shouldNotBeSuccessWrongEmptyName() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(4,"dd.MM.yyyy");
 
         $x("//input[@placeholder='Город']").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(ltdFormat);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("");
         $x("//input[@name='phone']").setValue("+74991112233");
         $("[data-test-id=agreement]").click();
@@ -185,13 +158,12 @@ public class CardDeliveryTest {
     }
     @Test
     void shouldNotBeSuccessWrongShortPhone() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(4,"dd.MM.yyyy");
 
         $x("//input[@placeholder='Город']").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(ltdFormat);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("Олег-Тиньков");
         $x("//input[@name='phone']").setValue("+7499111223");
         $("[data-test-id=agreement]").click();
@@ -201,13 +173,12 @@ public class CardDeliveryTest {
 
     @Test
     void shouldNotBeSuccessWrongLongPhone() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(4,"dd.MM.yyyy");
 
         $x("//input[@placeholder='Город']").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(ltdFormat);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("Олег-Тиньков");
         $x("//input[@name='phone']").setValue("+749911122333");
         $("[data-test-id=agreement]").click();
@@ -217,13 +188,12 @@ public class CardDeliveryTest {
 
     @Test
     void shouldNotBeSuccessWrongPhoneHasNotPlus() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(4,"dd.MM.yyyy");
 
         $x("//input[@placeholder='Город']").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(ltdFormat);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("Олег-Тиньков");
         $x("//input[@name='phone']").setValue("74991112233");
         $("[data-test-id=agreement]").click();
@@ -233,13 +203,12 @@ public class CardDeliveryTest {
 
     @Test
     void shouldNotBeSuccessEmptyPhone() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(4,"dd.MM.yyyy");
 
         $x("//input[@placeholder='Город']").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(ltdFormat);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("Олег-Тиньков");
         $x("//input[@name='phone']").setValue("");
         $("[data-test-id=agreement]").click();
@@ -249,13 +218,12 @@ public class CardDeliveryTest {
 
     @Test
     void shouldNotBeSuccessCheckMarkNotChecked() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(4,"dd.MM.yyyy");
 
         $x("//input[@placeholder='Город']").setValue("Москва");
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(ltdFormat);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("Олег-Тиньков");
         $x("//input[@name='phone']").setValue("+74991112233");
 //        $("[data-test-id=agreement]").click();
@@ -266,9 +234,8 @@ public class CardDeliveryTest {
 
     @Test
     void shouldBeSuccessDropDownListCity() {
-        LocalDateTime ldt = LocalDateTime.now().plusDays(3);
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String ltdFormat = format1.format(ldt);
+        String planningDate = generateDate(4,"dd.MM.yyyy");
+
         String rand = randomAlphabetic(2);
         char [] alphabet = {'а','б','в','г','д','е','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ш','ю'};
         int rNum1 = (int) (Math.random() * 22);
@@ -282,15 +249,16 @@ public class CardDeliveryTest {
         str = sb.toString();
 
 
-
         $x("//input[@placeholder='Город']").setValue(str);
         $(".menu-item__control").click();
-        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(ltdFormat);
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+        $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $x("//input[@name='name']").setValue("Олег-Тиньков");
         $x("//input[@name='phone']").setValue("+74991112233");
         $("[data-test-id=agreement]").click();
         $x("//*[contains(text(), 'Забронировать')]").click();
         $x("//*[contains(text(), 'Успешно!')]").should(Condition.appear, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate)).shouldBe(Condition.visible);
     }
 }
